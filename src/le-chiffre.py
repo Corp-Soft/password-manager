fefda3c07a46c02a1c11f40627afa0b28049237b
arturovt <arthurandrosovich@gmail.com>
2017-12-25 16:31:32 +0200

    passwords are removable

--- src/le-chiffre.py
+++ src/le-chiffre.py
@@ -9,11 +9,12 @@ Usage: le-chiffre COMMAND
 Fast and secure command line tool for generating random passwords
 
 Options:
-    -g, generate <url>    Generate random hash, store in encrypted file and copy to clipboard
-    -f, find <url>        Find necessary password for given URL and copy to clipboard
-    -l, list              List all available passwords
-    -v, version           Print version information and quit
-    set config min_password_length <number>'''
+    -g, generate <url>               Generate random hash, store in encrypted file and copy to clipboard
+    -f, find <url>                   Find necessary password for given URL and copy to clipboard
+    -l, list                         List all available passwords
+    -r, remove <url>                 Remove url for given URL
+    -v, version                      Print version information and quit
+    set min_password_length <number> Set minimum password length'''
 
 version = 'le-chiffre version 0.0.1@alpha'
 
@@ -44,18 +45,18 @@ def copy_to_clipboard(password):
 def get_username():
     return subprocess.getoutput('whoami')
 
+# read `min_password_length` property from json, otherwise default pass length is 10
 def get_min_password_length():
     username = get_username()
 
-    if os.path.exists(os.getcwd() + '/settings.json'):
-        data = json.load(open('settings.json'))
+    if not os.path.exists(os.getcwd() + '/settings.json'):
+        print('le-chiffre: Please create `settings.json` file!')
+        sys.exit(0)
 
-        if 'min_password_length' in data:
-            return data['min_password_length']
+    data = json.load(open('settings.json'))
 
-        else:
-            return 10
-
+    if 'min_password_length' in data:
+        return data['min_password_length']
     else:
         return 10
 
@@ -123,65 +124,94 @@ def find_password(url):
     print('le-chiffre: You\'re searching password for url {}'.format(url))
     username = get_username()
 
-    if os.path.exists('/home/{}/.le-chiffre'.format(username)):
-        encrypted = open('/home/{}/.le-chiffre/passwords.enc'.format(username)).read()
-
-        hash = api.get_hash()
-        passwords = json.loads(aes(hash).decrypt(encrypted))
-
-        if type(passwords) == list:
-            searchable_password = None
-
-            for i in passwords:
-                if i['url'] == url:
-                    searchable_password = i['password']
-
-            if searchable_password is not None and len(searchable_password) > 0:
-                print('le-chiffre: I\'ve found {}'.format(searchable_password))
-                copy_to_clipboard(searchable_password)
-            else:
-                print('le-chiffre: Sorry, I haven\'t found anything for that url!')
-
-    else:
+    if not os.path.exists('/home/{}/.le-chiffre'.format(username)):
         print('le-chiffre: You haven\'t generated any password yet to find anything!')
+        sys.exit(0)
+
+    encrypted = open('/home/{}/.le-chiffre/passwords.enc'.format(username)).read()
+
+    hash = api.get_hash()
+    passwords = json.loads(aes(hash).decrypt(encrypted))
+
+    if type(passwords) == list:
+        searchable_password = None
+
+        for i in passwords:
+            if i['url'] == url:
+                searchable_password = i['password']
+
+        if searchable_password is not None and len(searchable_password) > 0:
+            print('le-chiffre: I\'ve found {}'.format(searchable_password))
+            copy_to_clipboard(searchable_password)
+        else:
+            print('le-chiffre: Sorry, I haven\'t found anything for that url!')
 
 # list all available passwords
 def list_passwords():
     username = get_username()
 
-    if os.path.exists('/home/{}/.le-chiffre'.format(username)):
-        print('le-chiffre: List all passwords!')
-
-        encrypted = open('/home/{}/.le-chiffre/passwords.enc'.format(username)).read()
-
-        hash = api.get_hash()
-        passwords = json.loads(aes(hash).decrypt(encrypted))
-
-        if type(passwords) == list:
-            print('\n')
-            for i in passwords:
-                password = i['password']
-                url = i['url']
-                print('le-chiffre: password => {0}, url => {1}'.format(password, url))
-
-    else:
+    if not os.path.exists('/home/{}/.le-chiffre'.format(username)):
         print('le-chiffre: You haven\'t generated any password yet to list them!')
+        sys.exit(0)
+
+    print('le-chiffre: List all passwords!')
+
+    encrypted = open('/home/{}/.le-chiffre/passwords.enc'.format(username)).read()
+
+    hash = api.get_hash()
+    passwords = json.loads(aes(hash).decrypt(encrypted))
+
+    if type(passwords) == list:
+        print('\n')
+        for i in passwords:
+            password = i['password']
+            url = i['url']
+            print('le-chiffre: password => {0}, url => {1}'.format(password, url))
 
 # setup additional configuration
 def set_password_length(length):
+    if not os.path.exists(os.getcwd() + '/settings.json'):
+        print('le-chiffre: Please create `settings.json` file!')
+        sys.exit(0)
+
+    data = json.load(open('settings.json'))
+
+    data['min_password_length'] = length
+    settings_file = open('settings.json', 'w')
+    settings_file.write(json.dumps(data, sort_keys=True, indent=4))
+    settings_file.close()
+    print('le-chiffre: Established `min_password_length` to => {}'.format(length))
+
+def remove_password(url):
+    print('le-chiffre: You wanna delete password for url => {}'.format(url))
     username = get_username()
 
-    if os.path.exists(os.getcwd() + '/settings.json'):
-        data = json.load(open('settings.json'))
+    if not os.path.exists('/home/{}/.le-chiffre'.format(username)):
+        print('le-chiffre: You haven\'t generated any password yet to remove any!')
+        sys.exit(0)
 
-        data['min_password_length'] = length
-        settings_file = open('settings.json', 'w')
-        settings_file.write(json.dumps(data, sort_keys=True, indent=4))
-        settings_file.close()
-        print('le-chiffre: Established `min_password_length` to => {}'.format(length))
+    encrypted = open('/home/{}/.le-chiffre/passwords.enc'.format(username)).read()
 
-    else:
-        print('le-chiffre: Please create `settings.json` file and put `token` there!')
+    hash = api.get_hash()
+    passwords = json.loads(aes(hash).decrypt(encrypted))
+
+    if type(passwords) == list:
+        current_len = len(passwords)
+
+        for i in range(len(passwords)):
+            if passwords[i]['url'] == url:
+                passwords.pop(i)
+
+                passwords = aes(hash).encrypt(json.dumps(passwords))
+
+                passwords_file = open('/home/{}/.le-chiffre/passwords.enc'.format(username), 'w')
+                passwords_file.write(passwords.decode('utf-8'))
+                passwords_file.close()
+
+                print('le-chiffre: Deleted password for url => {}'.format(url))
+
+        if current_len == len(passwords):
+            print('le-chiffre: Sorry, I haven\'t found anything for that url!')
 
 def main(): 
     args = sys.argv
@@ -218,11 +248,17 @@ def main():
             else:
                 print('le-chiffre: You\'ve provided invalid url!')
 
+        elif option == '-r' or option == 'remove':
+            if parse_url(argument):
+                remove_password(argument)
+            else:
+                print('le-chiffre: You\'ve provided invalid url!')
+
         else:
             print('le-chiffre: You\'ve provided incorrent option!')
 
     # length of arguments for configuration
-    elif len(args) == 5:
+    elif len(args) == 4:
         option, argument = parse_config(args[-3:])
 
         if option == 'min_password_length':
